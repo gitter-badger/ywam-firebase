ba4c3b641cc5d59a375142a23e0bd8a15f0295d7
Tim Scholten <tim.scholten@gmail.com>
2017-04-20 09:07:07 -0400

    hide dashboard nav if not logged in

--- client/app/apply_app_route_components/apply/apply.controller.js
+++ client/app/apply_app_route_components/apply/apply.controller.js
@@ -4,6 +4,7 @@ class ApplyController {
    /* @ngInject */
   constructor($rootScope,$state, Site) {
           var ctrl = this;  
+              ctrl.site = Site
  
  $rootScope.$on('$viewContentLoaded',function(){ ctrl.current = $state.current.name})
       
