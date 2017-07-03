4308d52a0254e1877d200ffe90512d8acba61999
Natalie Wurster <natalie.wurster@ywamsarasota.com>
2017-07-03 16:44:55 -0400

    reference email changes

--- functions/references/sendEmail.js
+++ functions/references/sendEmail.js
@@ -174,7 +174,7 @@ function emailStep3(data){
     
         
       return nodemailerMailgun.sendMail({
-           from: 'info@ywamsarasota.com',
+           from:'"YWAM Sarasota" <info@ywamsarasota.com>',
            to: data.refData.user_set.email, // An array if you have multiple recipients.
            //cc:'second@domain.com',
            //bcc:'secretagent@company.gov',
